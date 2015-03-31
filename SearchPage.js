64925da804b9dd4c3e92f9e55a31bb8435a1cca8
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-03-31 20:28:54 +0100

    Added a search page

---
+++ SearchPage.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableHighlight,
+  ActivityIndicatorIOS,
+  Image,
+  ix,
+  Component
+} = React;
+
+var styles = StyleSheet.create({
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565'
+  },
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center'
+  }
+});
+
+class SearchPage extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          Search for houses to buy!
+        </Text>
+        <Text style={styles.description}>
+          Search by place-name, postcode or search near your location.
+        </Text>
+      </View>
+    );
+  }
+}
+
+module.exports = SearchPage;
--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,9 @@
 'use strict';
 
 var React = require('react-native');
+var SearchPage = require('./SearchPage');
 
 var styles = React.StyleSheet.create({
-  text: {
-    color: 'black',
-    backgroundColor: 'white',
-    fontSize: 30,
-    margin: 80
-  },
   container: {
     flex: 1
   }
@@ -21,7 +16,7 @@ class PropertyFinderApp extends React.Component {
         style={styles.container}
         initialRoute={{
           title: 'Property Finder',
-          component: HelloWorld,
+          component: SearchPage,
         }}/>
     );
   }
