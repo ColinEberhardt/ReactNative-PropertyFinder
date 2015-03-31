a0ec551129e8d739e91acce495ce5d44c4e117fa
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-03-31 20:27:29 +0100

    Added navigation controller

--- index.ios.js
+++ index.ios.js
@@ -8,10 +8,26 @@ var styles = React.StyleSheet.create({
     backgroundColor: 'white',
     fontSize: 30,
     margin: 80
+  },
+  container: {
+    flex: 1
   }
 });
 
 class PropertyFinderApp extends React.Component {
+  render() {
+    return (
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: HelloWorld,
+        }}/>
+    );
+  }
+}
+
+class HelloWorld extends React.Component {
   render() {
     return <React.Text style={styles.text}>Hello World (Again)</React.Text>;
   }
