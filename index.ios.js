e6ed711c45b70d11511d3f31c3211764ceccd358
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-03-31 20:18:40 +0100

    Hello World Example

--- index.ios.js
+++ index.ios.js
@@ -1,49 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
 
-var PropertyFinder = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js{'\n'}
-          Press Cmd+R to reload
-        </Text>
-      </View>
-    );
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
   }
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-  },
-});
+class PropertyFinderApp extends React.Component {
+  render() {
+    return React.createElement(React.Text, {style: styles.text}, 'Hello World!');
+  }
+}
 
-AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
+React.AppRegistry.registerComponent('PropertyFinder',
+  function() { return PropertyFinderApp });
