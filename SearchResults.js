cd5c1e279de3afd2c5e982d86a9363527ff5ed5c
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-03-31 20:38:43 +0100

    Added a PropertyView

--- SearchResults.js
+++ SearchResults.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react-native');
+var PropertyView = require('./PropertyView');
 var {
   StyleSheet,
   Image, 
@@ -53,6 +54,12 @@ class SearchResults extends Component {
   rowPressed(propertyGuid) {
     var property = this.props.listings
       .filter(prop => prop.guid === propertyGuid)[0];
+
+    this.props.navigator.push({
+      title: "Property",
+      component: PropertyView,
+      passProps: {property: property}
+    });
   }
 
   renderRow(rowData, sectionID, rowID) {
