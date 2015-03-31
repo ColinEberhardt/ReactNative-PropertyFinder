c157274aff2b0bd359c7e3e69dc8fac1db00fd6a
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-03-31 20:30:12 +0100

    Added buttons and text input

--- SearchPage.js
+++ SearchPage.js
@@ -24,7 +24,41 @@ var styles = StyleSheet.create({
     padding: 30,
     marginTop: 65,
     alignItems: 'center'
-  }
+  },
+  flowRight: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  alignSelf: 'stretch'
+},
+buttonText: {
+  fontSize: 18,
+  color: 'white',
+  alignSelf: 'center'
+},
+button: {
+  height: 36,
+  flex: 1,
+  flexDirection: 'row',
+  backgroundColor: '#48BBEC',
+  borderColor: '#48BBEC',
+  borderWidth: 1,
+  borderRadius: 8,
+  marginBottom: 10,
+  alignSelf: 'stretch',
+  justifyContent: 'center'
+},
+searchInput: {
+  height: 36,
+  padding: 4,
+  marginRight: 5,
+  flex: 4,
+  fontSize: 18,
+  borderWidth: 1,
+  borderColor: '#48BBEC',
+  borderRadius: 8,
+  color: '#48BBEC'
+}
+
 });
 
 class SearchPage extends Component {
@@ -37,6 +71,20 @@ class SearchPage extends Component {
         <Text style={styles.description}>
           Search by place-name, postcode or search near your location.
         </Text>
+        <View style={styles.flowRight}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder='Search via name or postcode'/>
+          <TouchableHighlight style={styles.button}
+              underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Go</Text>
+          </TouchableHighlight>
+        </View>
+        <TouchableHighlight style={styles.button}
+            underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Location</Text>
+        </TouchableHighlight>
+
       </View>
     );
   }
