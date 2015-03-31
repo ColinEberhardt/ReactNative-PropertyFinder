ef8a0cd3f2354d2eb8f9d4a427ca9fbcee0ddb9c
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-03-31 20:37:40 +0100

    Improved row rendering

--- SearchResults.js
+++ SearchResults.js
@@ -11,6 +11,34 @@ var {
   Component
 } = React;
 
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
+
 class SearchResults extends Component {
 
   constructor(props) {
@@ -22,12 +50,27 @@ class SearchResults extends Component {
     };
   }
 
+  rowPressed(propertyGuid) {
+    var property = this.props.listings
+      .filter(prop => prop.guid === propertyGuid)[0];
+  }
+
   renderRow(rowData, sectionID, rowID) {
+    var price = rowData.price_formatted.split(' ')[0];
+
     return (
-      <TouchableHighlight
+      <TouchableHighlight onPress={() => this.rowPressed(rowData.guid)}
           underlayColor='#dddddd'>
         <View>
-          <Text>{rowData.title}</Text>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+            <View  style={styles.textContainer}>
+              <Text style={styles.price}>£{price}</Text>
+              <Text style={styles.title} 
+                    numberOfLines={1}>{rowData.title}</Text>
+            </View>
+          </View>
+          <View style={styles.separator}/>
         </View>
       </TouchableHighlight>
     );
