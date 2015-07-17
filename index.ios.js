a38d8f43eaec5c3287b33ad15f1199c0f2f2e7d7
Daniel Ternyak <dternyak@gmail.com>
2015-07-16 21:56:53 -0500

    Removed Broken Class Implmenation.
    Because PropertyFinderApp extends React.Component, returning
    React.NavigatorIOS was breaking the router on my system.

--- index.ios.js
+++ index.ios.js
@@ -12,7 +12,7 @@ var styles = React.StyleSheet.create({
 class PropertyFinderApp extends React.Component {
   render() {
     return (
-      <React.NavigatorIOS
+      <NavigatorIOS
         style={styles.container}
         initialRoute={{
           title: 'Property Finder',
